284bd04247bf7c9bbd35acf93a98520f956c9d45
AlexisAndradeDev <alexis.martinez.contacto@gmail.com>
2024-11-14 14:14:03 -0600

    Agrega tests de Django

--- videonet_project/video_manager/tests.py
+++ videonet_project/video_manager/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Video
 
-# Create your tests here.
+class VideoModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.video = Video.objects.create(
+            title='Video Test',
+            description='Descripcion del video de prueba.',
+            video_file='videos/test_vid.mp4',
+            thumbnail='thumbnails/test_img.jpg',
+            user=self.user
+        )
+
+    def test_video_creation(self):
+        self.assertEqual(self.video.title, 'Video Test')
+        self.assertEqual(self.video.description, 'Descripcion del video de prueba.')
+        self.assertEqual(self.video.user.username, 'testuser')
+
+    def test_video_delete(self):
+        video_id = self.video.id
+        self.video.delete()
+        with self.assertRaises(Video.DoesNotExist):
+            Video.objects.get(id=video_id)
